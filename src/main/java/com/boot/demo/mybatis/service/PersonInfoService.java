62c0b06a01e1c66581d2c1d27722c27a3878c0de
Forward233 <18729041176@163.com>
2021-03-29 08:17:18 +0800

    daily practice

--- src/main/java/com/boot/demo/mybatis/service/PersonInfoService.java
+++ src/main/java/com/boot/demo/mybatis/service/PersonInfoService.java
@@ -5,6 +5,8 @@ import com.boot.demo.mybatis.entity.Person;
 import com.boot.demo.mybatis.mapper.PersonMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author: yhl
@@ -17,9 +19,23 @@ public class PersonInfoService {
     @Autowired
     private PersonMapper personMapper;
 
+    @Transactional(rollbackFor = Exception.class)
+    public void update() {
+        updatePerson();
+        Person person = getPersonById(2L);
+        System.out.println(person);
+    }
+
+    @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRES_NEW)
     public void updatePerson() {
         LambdaUpdateWrapper<Person> updateWrapper = new LambdaUpdateWrapper<>();
-        updateWrapper.set(Person::getAge, 15).eq(Person::getId, 2);
-        final int update = personMapper.update(null, updateWrapper);
+        updateWrapper.set(Person::getAge, 25).eq(Person::getId, 2);
+        personMapper.update(null, updateWrapper);
+        int i = 1 / 0;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public Person getPersonById(Long id) {
+        return personMapper.selectById(id);
     }
 }
--- src/test/java/com/boot/demo/mybatis/service/PersonServiceTest.java
+++ src/test/java/com/boot/demo/mybatis/service/PersonServiceTest.java
@@ -17,6 +17,6 @@ class PersonServiceTest {
 
     @Test
     void updatePerson() {
-        personInfoService.updatePerson();
+        personInfoService.update();
     }
 }
